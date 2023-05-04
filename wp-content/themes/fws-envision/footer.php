84ecb4359e6c69c17b71bd4d4c3ad5cb66f78153
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-05-04 16:57:46 +0200

    sub-footer completed

--- wp-content/themes/fws-envision/footer.php
+++ wp-content/themes/fws-envision/footer.php
@@ -14,9 +14,21 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="copyright">
-			<p>&copy; <?php echo date( 'Y' ); ?> Starter Theme</p>
-		</div><!-- .copyright -->
+		<div class="container"></div>
+		<div class="container">
+			<div class="sub-footer__container">
+				<div class="sub-footer__logo-box">
+					<a class="site-footer__logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img class="site-footer__logo-img" src="<?php echo fws()->images()->assetsSrc('footer-logo-img.png'); ?>" alt="<?php bloginfo( 'name' ); ?> Logo" title="<?php bloginfo( 'name' ); ?>">
+					</a>
+				</div>
+
+				<div class="sub-footer__copyright-box">
+					<p class="sub-footer__text">Copyright © 2023 Envision. All rights reserved. Project by  <a class="sub-footer__text sub-footer__text-fws" href="https://forwardslashny.com/" target="_blank">Forwardslash</a></p>
+				</div>
+			</div><!-- sub-footer__container -->
+		</div><!-- container -->
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
