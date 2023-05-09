eb9c3c6290b081b12463e3d41fbfbc879382fc7a
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-05-09 10:31:38 +0200

    tabs-section completed

--- wp-content/themes/fws-envision/src/js/site/tab-changer.js
+++ wp-content/themes/fws-envision/src/js/site/tab-changer.js
@@ -2,7 +2,6 @@
 const $ = jQuery.noConflict();
 
 /** @description import Global or some other files only if you need them */
-import Global from '../shared/global';
 
 'use strict';
 const TabChanger = {
@@ -32,7 +31,6 @@ const TabChanger = {
 
 		$('.js-tab-divider').on('click', function() {
 			const tabIndex = $(this).index();
-			console.log(tabIndex);
 
 			$('.js-tab-divider').removeClass('active');
 			$(this).addClass('active');
@@ -40,26 +38,8 @@ const TabChanger = {
 			$('.js-tab-card').removeClass('active');
 			$('.js-tab-card').eq(tabIndex).addClass('active');
 
-			/**
-			 * @description When keyword 'this' is no longer pointing to the main object, like in this scenario,
-				please use the variable name of the main object in order to access it( in an example below we are using 'ExampleFile.someFunction()'
-				instead of 'this.someFunction()').
-			 */
-			// ExampleFile.someFunction($(this));
-
-
 		});
 	},
-
-	someFunction: function(selector) {
-		/** @description Global.varsWindowWidth; - this variable is called from global.js file */
-		const ww = Global.varsWindowWidth;
-		const something = selector.attr(this.attrExampleDataAttr);
-
-		if (selector.hasClass(this.classExampleShow) && ww > 768) {
-			console.log(something);
-		}
-	}
 };
 
 export default TabChanger;
